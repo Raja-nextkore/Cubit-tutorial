60f5cadc5f25884e57694beeccf4227d134b2d99
Raja Ram <mr.raj00111777@gmail.com>
2022-09-27 10:28:12 +0530

    UI and logic connection done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:bloc_cubit_tutorial/cubit/internet_cubit/internet_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'pages/home_page.dart';
 
@@ -11,9 +13,13 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
+    return BlocBuilder<InternetCubit, InternetState>(
+      builder: (context, state) {
+        return const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: HomePage(),
+        );
+      },
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,44 @@
+import 'package:bloc_cubit_tutorial/cubit/internet_cubit/internet_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../cubit/internet_cubit/internet_cubit.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Loading...'),
+        child: BlocConsumer<InternetCubit, InternetState>(
+          listener: (context, state) {
+            if (state == InternetState.gainedState) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  backgroundColor: Colors.green,
+                  content: Text('Connected!'),
+                ),
+              );
+            } else {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  backgroundColor: Colors.red,
+                  content: Text('Not Connected!'),
+                ),
+              );
+            }
+          },
+          builder: (context, state) {
+            if (state == InternetState.gainedState) {
+              return const Text('Connected!');
+            } else if (state == InternetState.lostState) {
+              return const Text('Not Connected!');
+            } else {
+              return const Text('Loading...');
+            }
+          },
+        ),
       ),
     );
   }
